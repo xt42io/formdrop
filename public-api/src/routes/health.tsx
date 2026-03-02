33d111e47e4c5aa974d5b1cf311cffc2d3081c79
Akinkunmi <akinkunmioye42@gmail.com>
2026-03-02 14:57:35 +0100

    refactor: Migrate public API from React Serve JS to Express.js and update API documentation domain.

--- public-api/src/routes/health.tsx
+++
@@ -1,9 +0,0 @@
-import { Response, Route } from "react-serve-js";
-
-export const HealthCheckRoute = () => (
-  <Route path="/" method="GET">
-    {() => {
-      return <Response json={{ message: "FormDrop API v1.0" }} />;
-    }}
-  </Route>
-);

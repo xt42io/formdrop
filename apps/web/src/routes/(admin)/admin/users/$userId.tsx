0e27e600fd87375a2ffa4e079885a771a0d42cff
devtofunmi <olayiwolajesutofunmi@gmail.com>
2026-09-07 15:15:33 +0100

    refactor(web): import Button from @formdrop/ui in the admin user detail page
    One-line repoint. The component is identical -- it moved packages, it did not
    change -- so nothing about this screen behaves differently.

--- apps/web/src/routes/(admin)/admin/users/$userId.tsx
+++ apps/web/src/routes/(admin)/admin/users/$userId.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Button } from "@/components/button";
+import { Button } from "@formdrop/ui";
 import {
   ArrowLeft01Icon,
   UserIcon,
